c7dfc63e87633d5bf9b168073f2ead74b994544d
Sam <sam.saffron@gmail.com>
2015-10-12 14:40:45 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# RailsMultisite
+
+This gem provides multi-db support for Rails applications.
+
+Using its middleware you can partition your app so each hostname has its own db.
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+    gem 'rails_multisite'
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install rails_multisite
+
+## Usage
+
+TODO: Write usage instructions here
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Added some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
---
+++ spec/spec_helper.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'rails'
+require 'active_record'
+
+
+ENV["RAILS_ENV"] ||= 'test'
+RSpec.configure do |config|
+
+  require 'sqlite3'
+  class SQLite3::Database
+    def self.query_log
+      @@query_log ||= []
+    end
+
+    alias_method :old_execute, :execute
+    alias_method :old_prepare, :prepare
+
+    def execute(*args,&blk)
+      self.class.query_log << [args, caller, Thread.current.object_id]
+      old_execute(*args,&blk)
+    end
+
+    def prepare(*args,&blk)
+      self.class.query_log << [args, caller, Thread.current.object_id]
+      old_prepare(*args,&blk)
+    end
+
+  end
+
+  config.before(:suite) do
+    ActiveRecord::Base.configurations['test'] = (YAML::load(File.open("spec/fixtures/database.yml"))['test'])
+  end
+
+end
+
+
