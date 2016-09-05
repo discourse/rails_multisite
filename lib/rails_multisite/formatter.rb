b2483c946a215748e427bad6a87b39e0dc2dcfd9
Guo Xiang Tan <tgx_world@hotmail.com>
2016-09-05 11:51:24 +0800

    Include `ActiveSupport::TaggedLogging::Formatter` in `RailsMultisite::Formatter`.
    `ActiveSupport::TaggedLogging::Formatter` is included automatically when
    the logger is initialized but that happens during the :initialize_logger
    initializer where we can't configure the log_formatter yet.

--- lib/rails_multisite/formatter.rb
+++ lib/rails_multisite/formatter.rb
@@ -1,5 +1,7 @@
 module RailsMultisite
   class Formatter < ::ActiveSupport::Logger::SimpleFormatter
+    include ::ActiveSupport::TaggedLogging::Formatter
+
     def call(severity, timestamp, progname, msg)
       "[#{RailsMultisite::ConnectionManagement.current_db}] #{super}"
     end
