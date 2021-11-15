c6785cdb5c9277dd2c5ac8d55180dd1ece440ed0
David Taylor <david@taylorhq.com>
2021-11-15 15:32:45 +0000

    SECURITY: Vary the encrypted/signed cookie salts per-hostname (#26)
    This ensures Rails encrypted/signed cookies cannot be re-used across different sites which share the same secret_key_base

---
+++ lib/rails_multisite/cookie_salt.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module RailsMultisite
+  class CookieSalt
+    COOKIE_SALT_KEYS = [
+      "action_dispatch.signed_cookie_salt",
+      "action_dispatch.encrypted_cookie_salt",
+      "action_dispatch.encrypted_signed_cookie_salt",
+      "action_dispatch.authenticated_encrypted_cookie_salt"
+    ]
+
+    def self.update_cookie_salts(env:, host:)
+      COOKIE_SALT_KEYS.each { |key| env[key] = "#{env[key]} #{host}" }
+    end
+  end
+end
